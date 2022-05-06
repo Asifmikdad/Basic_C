7705d3a47cd0d047278ea22d50fdb88d6e9e3a62
Asifmikdad <asifmikdad@gmail.com>
2022-05-06 18:46:13 +0600

    variable naming

---
+++ variable_naming.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int potatoPrice;
+    int examMarks;
+
+    potatoPrice = 10;
+    examMarks = 76;
+
+    printf("Potato price is %d\n", potatoPrice);
+    printf("Exam marks is %d\n", examMarks);
+
+    return 0;
+}
+/*Variable naming rule:
+1. Upper case letter
+2. Lower case letter
+3. Underscore
+4. Digits 
+5. Never start with digits 
+
+Use camel case convention- it means first letter small then first letter of everyword is capital.
+*/
