d43787f8fb8ac4ff95b4fc618f716204da28df88
Asifmikdad <asifmikdad@gmail.com>
2022-05-06 18:31:04 +0600

    variable

---
+++ variable.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int price;
+    int marks;
+    float temparature;
+
+    price = 30;
+    marks = 60;
+    temparature = 36.58;
+
+    printf("Price is %d\n", price);
+    printf("Marks is %d\n", marks);
+    printf("Temparature is %f\n", temparature);
+
+
+
+    return 0;
+}
