41ea7d4eee79c925e1492197756f069f05c3980e
Asifmikdad <asifmikdad@gmail.com>
2022-05-07 20:22:12 +0600

    arithmatic update

--- arithmatic_operator.c
+++ arithmatic_operator.c
@@ -1,6 +1,20 @@
 #include<stdio.h>
 int main(int argc, char const *argv[])
 {
-    
+    int num1, num2, sum, sub, mul, div, rem;
+    printf("Enter two numbers: ");
+    scanf("%d%d", &num1, &num2);
+
+    sum = num1 + num2;
+    sub = num1 - num2;
+    mul = num1 * num2;
+    div = num1 / num2;
+    rem = num1 % num2;
+
+    printf("Summation      = %d\n", sum);
+    printf("Subtraction    = %d\n", sub);
+    printf("Multiplication = %d\n", mul);
+    printf("Division       = %d\n", div);
+    printf("Reminder       = %d\n", rem);
     return 0;
 }
