9be0c77ffea571ab85857cec7d6daa1a90d556fb
Asifmikdad <asifmikdad@gmail.com>
2022-05-07 20:02:33 +0600

    update data type

--- data_type.c
+++ data_type.c
@@ -1,6 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
 int main(int argc, char const *argv[])
 {
-    printf("Hello");
+    int price = 100;
+    printf("%d \n", price);
+    float temparature = 36.5;
+    printf("%f \n", temparature);
+    double temparature2 = 56.4;
+    printf("%lf\n", temparature2);
+    char character = 'c';
+    printf("%c \n", character);
+
     return 0;
 }
