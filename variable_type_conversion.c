e40f35dc037c472eabbb18ce4672145988e04569
Asifmikdad <asifmikdad@gmail.com>
2022-05-07 21:09:12 +0600

    variable type conversion

---
+++ variable_type_conversion.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    double realnumber = 10.6;
+    int number = 10;
+    printf("Realnumber is %lf\n", realnumber);
+    printf("Number is %d", (int)realnumber);
+
+    printf("Number is %d\n", number);
+    printf("Number is %f", (float)number);
+
+
+    return 0;
+}
